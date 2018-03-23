f4ee13914963c985007aa65b22708fd7bc288a02
Salim Eid <salim.eid@polymtl.ca>
2018-03-23 09:08:59 -0400

    results

--- Code/runner.sh
+++ Code/runner.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 echo "algo,fichier,temps" > ./results.csv
 
 for algo in {"progdyn","tabou","vorace","vorace","vorace","vorace","vorace","vorace","vorace","vorace","vorace","vorace"}; do
-    for ex in $(ls Dataset); do
-        t=$(./tp.sh -e Dataset/${ex} -a $algo -t)
+    for ex in $(ls dataset); do
+        t=$(./tp.sh -e dataset/${ex} -a $algo -t)
         echo $algo,$ex,$t
     done
 done >> results.csv
